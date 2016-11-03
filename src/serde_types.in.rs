b617b77e632bd46c21abb3f81eae28ebd9b757c4
rushmorem <rushmore@webenchanter.com>
2016-11-03 10:17:35 +0200

    Fix WriteStatus

--- src/serde_types.in.rs
+++ src/serde_types.in.rs
@@ -40,15 +40,14 @@ struct ReqlResponse {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct WriteStatus {
-    pub inserted: u32,
-    pub replaced: u32,
-    pub unchanged: u32,
-    pub skipped: u32,
-    pub deleted: u32,
-    pub errors: u32,
-    pub first_error: Option<String>,
-    pub generated_keys: Option<Vec<Uuid>>,
-    pub warnings: Option<String>,
-    pub changes: Option<Vec<Value>>,
-    _p: (),
+    inserted: u32,
+    replaced: u32,
+    unchanged: u32,
+    skipped: u32,
+    deleted: u32,
+    errors: u32,
+    first_error: Option<String>,
+    generated_keys: Option<Vec<Uuid>>,
+    warnings: Option<String>,
+    changes: Option<Vec<Value>>,
 }
