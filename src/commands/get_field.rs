4ada375df0b17ac30b745058cecbcd4be5b2bbf3
rushmorem <rushmore@webenchanter.com>
2016-12-20 12:16:58 +0200

    Add number_field command

--- src/commands/get_field.rs
+++ src/commands/get_field.rs
@@ -5,32 +5,39 @@ use ql2::proto::Term_TermType as TermType;
 use super::Client;
 use serde_json::value::ToJson;
 
-impl<O> Client<types::StreamSelection, O>
-    where O: ToJson + Clone
-{
-    pub fn string_field<T>(self, arg: T) -> Client<types::String, ()>
-        where T: Into<types::String>
-    {
-        super::make_cmd(TermType::GET_FIELD, Some(vec![arg.into()]), None, Some(self.cmd), self.errors)
+macro_rules! define {
+    ($name:ident returns $typ:ident) => {
+        impl<O> Client<types::StreamSelection, O>
+            where O: ToJson + Clone
+            {
+                pub fn $name<T>(self, arg: T) -> Client<types::$typ, ()>
+                    where T: Into<types::String>
+                    {
+                        super::make_cmd(TermType::GET_FIELD, Some(vec![arg.into()]), None, Some(self.cmd), self.errors)
+                    }
+            }
+
+        impl<O> Client<types::Object, O>
+            where O: ToJson + Clone
+            {
+                pub fn $name<T>(self, arg: T) -> Client<types::$typ, ()>
+                    where T: Into<types::String>
+                    {
+                        super::make_cmd(TermType::GET_FIELD, Some(vec![arg.into()]), None, Some(self.cmd), self.errors)
+                    }
+            }
+
+        impl<O> Client<types::Array, O>
+            where O: ToJson + Clone
+            {
+                pub fn $name<T>(self, arg: T) -> Client<types::$typ, ()>
+                    where T: Into<types::String>
+                    {
+                        super::make_cmd(TermType::GET_FIELD, Some(vec![arg.into()]), None, Some(self.cmd), self.errors)
+                    }
+            }
     }
 }
 
-impl<O> Client<types::Object, O>
-    where O: ToJson + Clone
-{
-    pub fn string_field<T>(self, arg: T) -> Client<types::String, ()>
-        where T: Into<types::String>
-    {
-        super::make_cmd(TermType::GET_FIELD, Some(vec![arg.into()]), None, Some(self.cmd), self.errors)
-    }
-}
-
-impl<O> Client<types::Array, O>
-    where O: ToJson + Clone
-{
-    pub fn array_field<T>(self, arg: T) -> Client<types::Array, ()>
-        where T: Into<types::String>
-    {
-        super::make_cmd(TermType::GET_FIELD, Some(vec![arg.into()]), None, Some(self.cmd), self.errors)
-    }
-}
+define! { string_field returns String }
+define! { number_field returns Number }
