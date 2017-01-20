00be40e024f4885f2e9c2e44e8b0a4bf28b599fa
rushmorem <rushmore@webenchanter.com>
2017-01-20 23:49:41 +0200

    Remove the aliases attribute

--- src/commands/get_field.rs
+++ src/commands/get_field.rs
@@ -1,10 +1,5 @@
 command! {
     /// Get a single field from an object
 
-    #[command(
-        get_field(
-            args(field = "T"),
-            aliases(g),
-        )
-    )]
+    #[command(get_field(args(field = "T")))]
 }
