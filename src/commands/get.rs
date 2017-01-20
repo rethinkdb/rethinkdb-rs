f1783456d30bc5866345440826edc201d5dabf9a
rushmorem <rushmore@webenchanter.com>
2017-01-20 22:59:24 +0200

    Disable object example for now

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -12,19 +12,19 @@ command! {
     /// r.table("posts").get("a9849eef-7176-4411-935b-79a6e3c56a74").run::<Post>();
     /// ```
     ///
-    /// # Example
+    /*// # Example
     ///
     /// Find a document and merge another document with it.
     ///
     /// ```reql,macros
     /// # struct Hero;
     /// # fn main() {
-    /// r.table("heroes").get(3).merge(obj! {
-    ///     powers: arr!["invisibility", "speed"],
-    /// }).run::<Hero>();
+    /// r.table("heroes").get(3).merge(args!({
+    ///     powers: ["invisibility", "speed"],
+    /// })).run::<Hero>();
     /// # }
     /// ```
-    ///
+    *///
     /// # Example
     ///
     /// Subscribe to a document’s [changefeed](https://rethinkdb.com/docs/changefeeds/ruby/).
--- src/macros.rs
+++ src/macros.rs
@@ -15,102 +15,6 @@ macro_rules! command {
     }
 }
 
-/// Construct a ReQL JSON object from a Rust expression
-///
-/// # Example
-///
-/// Objects wrapped with expr can then be manipulated by ReQL API functions.
-///
-/// ```
-/// # #[macro_use] extern crate reql;
-/// # use reql::commands::*;
-/// # use reql::commands::run::Dummy;
-/// # use reql::r;
-/// # struct Value;
-/// # fn main() {
-/// expr!(obj!{a: "b"}).merge(obj!{b: arr![1, 2, 3]}).run::<Value>();
-/// # }
-/// ```
-///
-/// # Example
-///
-/// The `expr` macro can also compile an if else statement down to a ReQL branch command.
-///
-/// ```text
-/// # #[macro_use] extern crate reql;
-/// # use reql::commands::*;
-/// # use reql::commands::run::Dummy;
-/// # use reql::r;
-/// # struct Value;
-/// # fn main() {
-/// let hero = r.table("marvel").get("Iron Man");
-///
-/// r.do_(expr!{
-///
-///     if hero.get_field("victories") > 100 {
-///         hero.get_field("name") + " is a superhero"
-///     }
-///
-///     else if hero.get_field("victories") > 10 {
-///         hero.get_field("name") + " is a hero"
-///     }
-///
-///     else {
-///         hero.get_field("name") + " is very nice"
-///     }
-///
-/// }).run::<String>();
-/// # }
-/// ```
-#[macro_export]
-macro_rules! expr {
-    () => {};
-
-    (trace if $($cond:tt)* { $($body:tt)* } $(else if $($elif_cond:tt)* { $($elif_body:tt)* })* else { $($el_body:tt)* } $($tail:tt)*) => {{
-        expr!( r.branch(args!($($cond)*, $($body)*, $($($elif_cond)*, $($elif_body)*),* $($el_body)*)) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* && $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).and($($right)*) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* || $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).or($($right)*) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* == $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).eq($($right)*) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* != $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).ne($($right)*) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* > $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).gt($($right)*) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* >= $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).ge($($right)*) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* < $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).lt($($right)*) $($tail)*)
-    }};
-
-    ( trace $($left:tt)* <= $($right:tt)* $($tail:tt)* ) => {{
-        expr!( expr!($($left)*).le($($right)*) $($tail)*)
-    }};
-
-    ( trace ! $($cond:tt)* $($tail:tt)* ) => {{
-        expr!( r.not($($cond)*) $($tail)*)
-    }};
-
-    ($( $val:expr ),* $(,)*) => {{
-        $crate::Command::new(arr!($($val),*))
-    }};
-}
-
 /// Splice an array of arguments into another term
 ///
 /// `args` is a macro that’s used to splice a number of arguments into another term. This is
@@ -131,8 +35,7 @@ macro_rules! expr {
 /// # use reql::r;
 /// # fn main() {
 /// let x = 10;
-//// r.branch(args!(x > 5, "big", "small")).run::<String>();
-/// r.branch(args!(r.table(x) > 5, "big", "small")).run::<String>();
+/// r.branch(args!(x > 5, "big", "small")).run::<String>();
 /// # }
 /// ```
 ///
@@ -141,36 +44,38 @@ macro_rules! expr {
 /// * [arr](macro.arr.html)
 #[macro_export]
 macro_rules! args {
+    () => {};
+
     (trace $($left:tt)* && $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).and($($right)*), $($tail)*)
+        args!(args!($($left)*).and($($right)*), $($tail)*)
     }};
 
     (trace $($left:tt)* || $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).or($($right)*), $($tail)*)
+        args!(args!($($left)*).or($($right)*), $($tail)*)
     }};
 
     (trace $($left:tt)* == $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).eq($($right)*), $($tail)*)
+        args!(args!($($left)*).eq($($right)*), $($tail)*)
     }};
 
     (trace $($left:tt)* != $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).ne($($right)*), $($tail)*)
+        args!(args!($($left)*).ne($($right)*), $($tail)*)
     }};
 
-    (trace $left:block > $right:block, $($tail:tt)* ) => {{
-        args!(expr!($left).gt($right), $($tail)*)
+    (trace $($left:tt)* > $($right:tt)*, $($tail:tt)* ) => {{
+        args!(args!($($left)*).gt($($right)*), $($tail)*)
     }};
 
     (trace $($left:tt)* >= $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).ge($($right)*), $($tail)*)
+        args!(args!($($left)*).ge($($right)*), $($tail)*)
     }};
 
     (trace $($left:tt)* < $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).lt($($right)*), $($tail)*)
+        args!(args!($($left)*).lt($($right)*), $($tail)*)
     }};
 
     (trace $($left:tt)* <= $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).le($($right)*), $($tail)*)
+        args!(args!($($left)*).le($($right)*), $($tail)*)
     }};
 
     (trace ! $cond:tt, $($tail:tt)* ) => {{
@@ -178,43 +83,16 @@ macro_rules! args {
     }};
 
     (trace $($left:tt)* + $($right:tt)*, $($tail:tt)* ) => {{
-        args!(expr!($($left)*).add($($right)*), $($tail)*)
+        args!(args!($($left)*).add($($right)*), $($tail)*)
     }};
 
-    ($( $val:expr ),* $(,)*) => {{
-        arr!($($val),*)
-    }};
-}
-
-/// Take one or more values as arguments and return an array
-#[macro_export]
-macro_rules! arr {
-    ($( $val:expr ),* $(,)*) => {{
-        use $crate::ToArg;
-        use $crate::Term;
-
-        let mut term = Term::new();
-        $(
-            for arg in $val.to_arg() {
-                term.mut_args().push(arg);
-            }
-        )*
-        term
-    }}
-}
-
-/// Create an object from a list of key-value pairs, where the keys must be strings
-#[macro_export]
-macro_rules! obj {
-    ( $( $key:ident: $val:expr ),* $(,)*) => {{
-        use $crate::ToArg;
-        use $crate::Term;
-        use $crate::TermPair;
+    ( trace { $( $key:ident: $($val:tt)+ ),* } $(,)* $($tail:tt)* ) => {{
+        use $crate::{ToArg, Term, TermPair};
 
         let mut object = Term::new();
         $(
             let mut term = Term::new();
-            for arg in $val.to_arg() {
+            for arg in args!($($val)+).to_arg() {
                 term.mut_args().push(arg);
             }
             let key = stringify!($key);
@@ -223,6 +101,30 @@ macro_rules! obj {
             term_pair.set_val(term);
             object.mut_optargs().push(term_pair);
          )*
-        object
-    }}
+        args!(args!(object), $($tail)*)
+    }};
+
+    ( trace [ $( $val:expr ),* $(,)* ] $(,)* $($tail:tt)* ) => {{
+        use $crate::{ToArg, Term};
+
+        let mut term = Term::new();
+        $(
+            for arg in $val.to_arg() {
+                term.mut_args().push(arg);
+            }
+        )*
+        args!(args!(term), $($tail)*)
+    }};
+
+    ($( $val:expr ),* $(,)*) => {{
+        use $crate::{ToArg, Term, Command};
+
+        let mut term = Term::new();
+        $(
+            for arg in $val.to_arg() {
+                term.mut_args().push(arg);
+            }
+        )*
+        Command::new(term)
+    }};
 }
