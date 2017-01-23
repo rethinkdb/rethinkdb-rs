a90bcf836da73141b0d487a4db6711fdf6d0634c
rushmorem <rushmore@webenchanter.com>
2017-01-23 09:29:20 +0200

    args: make trailing commas optional

--- src/macros.rs
+++ src/macros.rs
@@ -40,11 +40,11 @@ macro_rules! command {
 /// ```
 #[macro_export]
 macro_rules! args {
-    ( $($arg:expr),* $(,)* ) => {{
+    ( $($arg:tt)* ) => {{
         use $crate::{ToArg, Command, Term};
 
         let mut term = Term::new();
-        __process_args!(term, $(($arg))*);
+        __process_args!(term, $($arg)*);
         Command::new(term)
     }};
 }
@@ -54,20 +54,22 @@ macro_rules! args {
 macro_rules! __process_args {
     ( $term:ident, ) => { };
     
-    ( $term:ident, ({ $($key:ident; $($val:tt)+),* $(,)* }) ) => {{
+    ( $term:ident,  $(,)* { $($key:ident: $($val:tt)+),* $(,)* } $(,)* ) => {{
         $(
             let key = stringify!($key);
-            let val = args!($($val)+);
+            let mut val = Term::new();
+            __process_args!(val, $($val)+);
             let temp_pair = Command::create_term_pair(key, val);
             $term.mut_optargs().push(temp_pair);
          )*
     }};
     
-    ( $term:ident, ({ $($key:ident; $($val:tt)+),* $(,)* }) $($tail:tt)+ ) => {{
+    ( $term:ident,  $(,)* { $($key:ident: $($val:tt)+),* $(,)* } $($tail:tt)* ) => {{
         let mut arg = Term::new();
         $(
             let key = stringify!($key);
-            let val = args!($($val)+);
+            let mut val = Term::new();
+            __process_args!(val, $($val)+);
             let temp_pair = Command::create_term_pair(key, val);
             arg.mut_optargs().push(temp_pair);
          )*
@@ -75,22 +77,28 @@ macro_rules! __process_args {
         __process_args!($term, $($tail)*);
     }};
     
-    ( $term:ident, ([ $($val:expr),* $(,)* ]) $($tail:tt)* ) => {{
+    ( $term:ident,  $(,)* [ $($val:expr),* ] $($tail:tt)* ) => {{
         let mut arg = Term::new();
         $(
-            arg.mut_args().push(args!($val).to_arg());
+            let mut val = Term::new();
+            __process_args!(val, $val);
+            arg.mut_args().push(val);
         )*
         $term.mut_args().push(arg);
         __process_args!($term, $($tail)*);
     }};
     
-    ( $term:ident, (| $($arg:ident),* $(,)* | { $body:tt }) $($tail:tt)* ) => {{
+    ( $term:ident,  $(,)* | $($arg:ident),* $(,)* | $body:tt $($tail:tt)* ) => {{
         unimplemented!();
         __process_args!($term, $($tail)*);
     }};
     
-    ( $term:ident, ($arg:expr) $($tail:tt)* ) => {{
+    ( $term:ident,  $(,)* $arg:expr, $($tail:tt)+ ) => {{
         $term.mut_args().push($arg.to_arg());
-        __process_args!($term, $($tail)*);
+        __process_args!($term, $($tail)+);
+    }};
+    
+    ( $term:ident,  $(,)* $arg:expr $(,)* ) => {{
+        $term.mut_args().push($arg.to_arg());
     }};
 }
