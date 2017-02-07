090126754b10a425c8510b57836c2a32845cc115
rushmorem <rushmore@webenchanter.com>
2017-02-07 17:26:33 +0200

    Add io traits

---
+++ src/commands/io/mod.rs
@@ -0,0 +1,19 @@
+use Result;
+use commands::Args;
+
+pub struct Response<T>(T);
+
+/// Create a new connection to the database server
+pub trait Connect {
+    type ConnectArgs;
+    type Connection;
+
+    fn connect(&self, args: Self::ConnectArgs) -> Result<Self::Connection>;
+}
+
+/// Run the query
+pub trait Run : Connect {
+    type RunArgs;
+
+    fn run<T>(&self, args: &Self::RunArgs) -> Response<T>;
+}
