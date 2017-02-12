3a18f439451baffc10cb4d0ef640a397bef20b39
rushmorem <rushmore@webenchanter.com>
2017-02-12 21:23:43 +0200

    Make sure docs-origin is defined

--- update-docs.sh
+++ update-docs.sh
@@ -5,6 +5,9 @@
 	# Go to top level directory
 	cd "$(dirname "$0")"
 
+	# Make sure the `docs-origin` remote is defined
+	git remote add docs-origin https://github.com/rethinkdb/docs.git &>/dev/null
+
 	# Pull the latest docs
 	git subtree pull --prefix build/docs docs-origin master
 )
