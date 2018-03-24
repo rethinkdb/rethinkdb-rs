21f89422366ec7975e1ac80c757ce23f50efc64a
rushmorem <rushmore@webenchanter.com>
2018-03-24 22:20:49 +0200

    Update blocking example

--- examples/blocking.rs
+++ examples/blocking.rs
@@ -2,11 +2,11 @@ extern crate futures_await as futures;
 #[macro_use]
 extern crate reql;
 extern crate reql_types;
-extern crate tokio;
 
-use futures::stream::Stream;
 use reql::{Client, Document, Run};
 use reql_types::ServerStatus;
+use futures::executor::block_on;
+use futures::StreamExt;
 
 fn main() {
     // Create a new ReQL client
@@ -22,30 +22,24 @@ fn main() {
         .unwrap();
 
     // Process the results
-    for res in stati.wait() {
-        match res {
-            // The server returned the response we were expecting
-            Ok(Some(Document::Expected(status))) => {
-                println!("{:?}", status);
-            }
-            // We got a response alright, but it wasn't the one we were
-            // expecting plus it's not an error either, otherwise it would
-            // have been returned as such (This simply means that the response
-            // we got couldn't be serialised into the type we were expecting)
-            Ok(Some(Document::Unexpected(status))) => {
-                println!("unexpected response from server: {:?}", status);
-            }
-            // This is impossible in this particular example since there
-            // needs to be at least one server available to give this
-            // response otherwise we would have run into an error for
-            // failing to connect
-            Ok(None) => {
-                println!("got no documents in the database");
-            }
-            // Our query ran into an error
-            Err(error) => {
-                println!("{:?}", error);
-            }
+    match block_on(stati.into_future()).unwrap().0.unwrap() {
+        // The server returned the response we were expecting
+        Some(Document::Expected(status)) => {
+            println!("{:?}", status);
+        }
+        // We got a response alright, but it wasn't the one we were
+        // expecting plus it's not an error either, otherwise it would
+        // have been returned as such (This simply means that the response
+        // we got couldn't be serialised into the type we were expecting)
+        Some(Document::Unexpected(status)) => {
+            println!("unexpected response from server: {:?}", status);
+        }
+        // This is impossible in this particular example since there
+        // needs to be at least one server available to give this
+        // response otherwise we would have run into an error for
+        // failing to connect
+        None => {
+            println!("got no documents in the database");
         }
     }
 }
