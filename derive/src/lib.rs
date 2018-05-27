bc781df119348a5abe38e61b1ae12f9b400c5d75
rushmorem <rushmore@webenchanter.com>
2018-05-27 15:31:39 +0200

    update derive crate

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,15 +1,19 @@
-#[macro_use]
-extern crate proc_macro_hack;
+#![feature(proc_macro)]
+
 #[macro_use]
 extern crate quote;
 extern crate syn;
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
 
 mod args;
 
-proc_macro_expr_impl! {
-    pub fn args_impl(input: &str) -> String {
-        args::Args::new(input)
-            .process().tokens
-            .to_string()
-    }
+#[proc_macro]
+pub fn args_impl(input: TokenStream) -> TokenStream {
+    args::Args::new(&input.to_string())
+        .process().tokens
+        .as_str()
+        .parse()
+        .unwrap()
 }
