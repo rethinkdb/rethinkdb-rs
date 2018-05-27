e10426ca49e5c43d8524f834ff86dcf0342c7324
rushmorem <rushmore@webenchanter.com>
2018-05-27 14:54:50 +0200

    update nix cfg

--- default.nix
+++ default.nix
@@ -3,7 +3,6 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "reql";
   buildInputs = [ gcc openssl gnumake inotify-tools ];
-  CARGO_INCREMENTAL = 1;
   OPENSSL_DIR = "${openssl.dev}";
   OPENSSL_LIB_DIR = "${openssl.out}/lib";
 }
