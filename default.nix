ebadc3f8e57cc0339447285c374ca99b1eb43620
rushmorem <rushmore@webenchanter.com>
2021-01-26 17:20:36 +0200

    wipe slate clean

--- default.nix
+++
@@ -1,8 +0,0 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "reql";
-  buildInputs = [ gcc openssl gnumake inotify-tools ];
-  OPENSSL_DIR = "${openssl.dev}";
-  OPENSSL_LIB_DIR = "${openssl.out}/lib";
-}
