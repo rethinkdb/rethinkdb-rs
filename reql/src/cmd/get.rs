f6510926f119f23d24e9d19183401a98e987b897
rushmorem <rushmore@webenchanter.com>
2021-05-21 12:33:10 +0200

    wip

--- reql/src/cmd/get.rs
+++ reql/src/cmd/get.rs
@@ -20,3 +20,19 @@ where
         Command::from_json(self).arg()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{cmd, r};
+
+    #[test]
+    fn r_db_table_get() {
+        let query = r
+            .db("foo")
+            .table("bar")
+            .get("84fc23ac-9e85-43af-b6f7-f86be17237e1");
+        let serialised = cmd::serialise(&query);
+        let expected = r#"[16,[[15,[[14,["foo"]],"bar"]],"84fc23ac-9e85-43af-b6f7-f86be17237e1"]]"#;
+        assert_eq!(serialised, expected);
+    }
+}
