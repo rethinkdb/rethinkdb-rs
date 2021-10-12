0170ae31eaa30789e5ac36aba7092a4191b0dc00
rushmorem <rushmore@webenchanter.com>
2021-10-12 18:18:28 +0200

    fix get_all

--- reql/src/cmd/get_all.rs
+++ reql/src/cmd/get_all.rs
@@ -1,14 +1,22 @@
 use super::args::Args;
-use super::index::Index;
 use crate::{cmd, Command};
 use ql2::term::TermType;
+use reql_macros::CommandOptions;
+use serde::Serialize;
+use std::borrow::Cow;
+
+#[derive(Debug, Clone, CommandOptions, Serialize, Default, PartialEq, PartialOrd)]
+#[non_exhaustive]
+pub struct Options {
+    pub index: Option<Cow<'static, str>>,
+}
 
 pub trait Arg {
-    fn arg(self) -> cmd::Arg<()>;
+    fn arg(self) -> cmd::Arg<Options>;
 }
 
 impl Arg for Command {
-    fn arg(self) -> cmd::Arg<()> {
+    fn arg(self) -> cmd::Arg<Options> {
         Self::new(TermType::GetAll).with_arg(self).into_arg()
     }
 }
@@ -17,15 +25,15 @@ impl<T> Arg for T
 where
     T: Into<String>,
 {
-    fn arg(self) -> cmd::Arg<()> {
+    fn arg(self) -> cmd::Arg<Options> {
         Command::from_json(self.into()).arg()
     }
 }
 
-impl Arg for Args<(&str, Index)> {
-    fn arg(self) -> cmd::Arg<()> {
-        let Args((key, Index(index))) = self;
-        key.arg().with_arg(index)
+impl Arg for Args<(&str, Options)> {
+    fn arg(self) -> cmd::Arg<Options> {
+        let Args((key, opts)) = self;
+        key.arg().with_opts(opts)
     }
 }
 
@@ -35,7 +43,7 @@ impl<T, const N: usize> Arg for Args<[T; N]>
 where
     T: Into<String> + Clone,
 {
-    fn arg(self) -> cmd::Arg<()> {
+    fn arg(self) -> cmd::Arg<Options> {
         let Args(arr) = self;
         let mut query = Command::new(TermType::GetAll);
         // TODO get rid of the clone in Rust v1.53
@@ -49,18 +57,18 @@ where
 
 #[allow(array_into_iter)]
 #[allow(clippy::into_iter_on_ref)]
-impl<T, const N: usize> Arg for Args<([T; N], Index)>
+impl<T, const N: usize> Arg for Args<([T; N], Options)>
 where
     T: Into<String> + Clone,
 {
-    fn arg(self) -> cmd::Arg<()> {
-        let Args((arr, Index(index))) = self;
+    fn arg(self) -> cmd::Arg<Options> {
+        let Args((arr, opts)) = self;
         let mut query = Command::new(TermType::GetAll);
         // TODO get rid of the clone in Rust v1.53
         for arg in arr.into_iter().cloned() {
             let arg = Command::from_json(arg.into());
             query = query.with_arg(arg);
         }
-        query.with_arg(index).into_arg()
+        query.with_opts(opts).into_arg()
     }
 }
